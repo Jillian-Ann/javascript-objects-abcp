2c27d3822b731d63fc072642138e0fdeacd23f4b
Jillian Mclaren <jillian.mclaren@gmail.com>
2018-01-20 16:14:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,14 @@
-var playlist = { outkast : 'roses'}
-function updatePlaylist (playlist,artist,song) {  playlist[artist]=song
-return playlist}
+var playlist = {
+  'Foo Fighters': 'Everlong',
+  Prince: 'Purple Rain'
+}
+
+function updatePlaylist(playlist, artist, song) {
+  // Remember, because one of this function's arguments
+  // is `playlist`, the `playlist` _inside_ the function
+  // is not the same as the top-level `playlist` outside
+  // the function
+  playlist[artist] = song
+
+  return playlist
+}
