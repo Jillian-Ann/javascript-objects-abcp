cdd7d6a97562fa4ab34cd0efb4375ad8837a28dc
Jillian Mclaren <jillian.mclaren@gmail.com>
2018-01-20 16:13:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 var playlist = { outkast : 'roses'}
-function updatePlaylist (playlist,artist,song) {  playlist.artist=song
+function updatePlaylist (playlist,artist,song) {  playlist[artist]=song
 return playlist}
